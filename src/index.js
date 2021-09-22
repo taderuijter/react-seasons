9b26b26e7bddca043b709ec29fc3ba8963d34118
Tadeusz de Ruijter <tadeuszderuijter@gmail.com>
2021-09-22 13:09:43 +0200

    Add Project setup changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// Adding polyfill for old browsers
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+// Adds react to the file
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Main Component for the app
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
